e57bae40e3f08d1fda23b1a4c09f55ee21e99cb9
Anna <annalordkemp@gmail.com>
2023-01-26 17:27:36 -0700

    adds fullCalendar extensions and basic calendar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Calendar from './Components/Calendar.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar></Calendar>
     </div>
   );
 }
---
+++ src/Components/Calendar.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import daygridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import './Calendar.css';
+
+const Calendar = () => {
+
+  const [events, setEvents] = useState([])
+
+  return (
+    <div id="full-calendar">
+      <FullCalendar 
+        editable
+        selectable
+        events={events}
+        headerToolbar={{
+          start: "today prev next",
+          end: "dayGridMonth dayGridWeek dayGridDay",
+        }}
+        plugins={[daygridPlugin, interactionPlugin]} 
+        views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+      />
+    </div>
+  )
+};
+
+export default Calendar;
