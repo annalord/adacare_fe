ed32683d93e4fb3e5a4d54f2e2526fdd9780498e
Anna <annalordkemp@gmail.com>
2023-02-01 17:14:25 -0700

    change index.js to use cloneElements to pass props down

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
@@ -7,12 +7,27 @@ import { routes } from './Router'
 
 const router = createBrowserRouter(routes);
 
+const Index = () => {
+  const [appState, setAppState] = useState({});
+
+  return (
+    <RouterProvider router={router}>
+      <React.StrictMode>
+        {routes.map(({ path, element }) => {
+          //clone an element and pass new props to it 
+          return React.cloneElement(element, {
+            key: path,
+            appState,
+            setAppState
+          });
+        })}
+      </React.StrictMode>
+    </RouterProvider>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<Index />);
 
 
 
