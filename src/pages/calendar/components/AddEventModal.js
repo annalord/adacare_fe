4706fc568ffc34e1e99280c362472b3934b64abc
Anna <annalordkemp@gmail.com>
2023-02-08 18:56:26 -0700

    fix issue with details and end datetime not accepting null

--- src/pages/calendar/components/AddEventModal.js
+++ src/pages/calendar/components/AddEventModal.js
@@ -1,42 +1,47 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-// import FormCheck from 'react-bootstrap/FormCheck'
 import { useState } from 'react';
-import { postEventApi } from '../../../api/EventsAPI.js'
+import { postEventApi } from '../../../api/EventsAPI.js';
 import { useAuthContext } from '../../../hooks/useAuthContext';
 
-
 const kDefaultFormState = {
-  title: "",
-  details: "",
-  start: "",
-  end: false,
-  allDay: false
+  title: '',
+  details: '',
+  start: '',
+  end: '',
+  allDay: false,
 };
 
+const prepEventForSubmit = (eventData) => {
+  if (eventData.end === '') {
+    eventData.end = '1970-01-01 00:01';
+  }
+  if (eventData.details === '') {
+    eventData.details = 'no details';
+  }
+
+  return eventData;
+};
 
 const AddEventModal = (props) => {
-
   const [formData, setFormData] = useState(kDefaultFormState);
   const { user } = useAuthContext();
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setFormData(kDefaultFormState);
     props.handleClose();
-    await postEventApi(formData, user.id); //post note to database 
+    await postEventApi(prepEventForSubmit(formData), user.id); //post note to database
     props.getAllEvents(); // get data again, updates state to rerender
   };
-
 
   const handleChange = (event) => {
     const fieldValue = event.target.value;
     const fieldName = event.target.name;
     // console.log(`${fieldName}: ${fieldValue}`)
     const newFormData = { ...formData, [fieldName]: fieldValue };
-    setFormData(newFormData)
+    setFormData(newFormData);
   };
 
   const handleAllDayChange = (event) => {
@@ -44,9 +49,8 @@ const AddEventModal = (props) => {
     const fieldName = event.target.name;
     // console.log(`${fieldName}: ${fieldValue}`)
     const newFormData = { ...formData, [fieldName]: fieldValue };
-    setFormData(newFormData)
+    setFormData(newFormData);
   };
-
 
   return (
     <Modal show={props.isOpen} onHide={props.handleClose}>
@@ -55,73 +59,70 @@ const AddEventModal = (props) => {
       </Modal.Header>
 
       <Modal.Body>
-        
         <Form>
-
           <Form.Group className='mb-1 mt-1'>
             <Form.Control
-                  placeholder='Event title'
-                  size='sm'
-                  name='title'
-                  value={formData.title}
-                  onChange={handleChange}
-                />
+              placeholder='Event title'
+              size='sm'
+              name='title'
+              value={formData.title}
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Form.Group className='mb-1'>
             <Form.Control
-                  placeholder='Event details'
-                  as='textarea'
-                  size='sm'
-                  name='details'
-                  value={formData.details}
-                  onChange={handleChange}
-                />
+              placeholder='Event details'
+              as='textarea'
+              size='sm'
+              name='details'
+              value={formData.details}
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Form.Group className='mb-1'>
             <Form.Check
-                  name='allDay'
-                  label='All day'
-                  id='all day checkbox'
-                  onChange={handleAllDayChange}
-                />
+              name='allDay'
+              label='All day'
+              id='all day checkbox'
+              onChange={handleAllDayChange}
+            />
           </Form.Group>
 
           <Form.Group className='mb-1'>
             <Form.Control
-                  placeholder='Start time and date'
-                  size='sm'
-                  name='start'
-                  value={formData.start}
-                  onChange={handleChange}
-                />
+              placeholder='Start time and date'
+              size='sm'
+              name='start'
+              value={formData.start}
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Form.Group className='mb-1'>
             <Form.Control
-                  placeholder='End time and date'
-                  size='sm'
-                  name='end'
-                  disabled={formData.allDay}
-                  value={formData.end}
-                  onChange={handleChange}
-                />
+              placeholder='End time and date'
+              size='sm'
+              name='end'
+              disabled={formData.allDay}
+              value={formData.end}
+              onChange={handleChange}
+            />
           </Form.Group>
         </Form>
-
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.handleClose}>
+        <Button variant='secondary' onClick={props.handleClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={handleSubmit}> 
-          Add Event 
+        <Button variant='primary' onClick={handleSubmit}>
+          Add Event
         </Button>
       </Modal.Footer>
-  </Modal>
-  )
+    </Modal>
+  );
 };
 
 export default AddEventModal;
